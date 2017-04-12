133cd4f7cec11f6a6af18a9a2798ac455c6fba0a
NeilWang <neilwang@objectivasoftware.com>
2017-04-12 11:33:59 +0800

    add solution

---
+++ DocumentManagement/DoucmentManagementWeb/Controllers/DocumentController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DocumentManagementCommon;
+
+namespace DoucmentManagementWeb.Controllers
+{
+    public class DocumentController : Controller
+    {
+        public ActionResult DocumentList()
+        {
+            return View(new List<DocumentInfo>());
+        }
+
+        public ActionResult AddDocument()
+        {
+            return View();
+        }
+    }
+}
---
+++ DocumentManagement/documentManagementAdminWeb/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace documentManagementAdminWeb
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
